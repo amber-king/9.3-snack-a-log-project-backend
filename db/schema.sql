1de738692a7a1e6725803cabb89ffee055b368aa
amber-king <amberking@pursuit.org>
2023-05-09 11:33:19 -0400

    added db files: dbConfig,schema,seed via psql

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+DROP DATABASE IF EXISTS snacks_dev;
+
+CREATE DATABASE snacks_dev;
+
+\ c snacks_dev;
+
+CREATE TABLE snacks (
+    snack_id SERIAL PRIMARY KEY,
+    snack_name TEXT NOT NULL,
+    time_eaten TEXT,
+    food_group TEXT,
+    is_healthy BOOLEAN,
+    calorie_count INT
+);
---
+++ db/seed.sql
@@ -0,0 +1,45 @@
+\ c snacks_dev
+INSERT INTO
+    snacks(
+        snack_name,
+        time_eaten,
+        food_group,
+        is_healthy,
+        calorie_count
+    )
+VALUES
+    (
+        'Popcorn',
+        'Evening',
+        'Grain',
+        true,
+        '106'
+    ),
+    (
+        'Chips',
+        'Afternoon',
+        'Snack',
+        false,
+        '152'
+    ),
+    (
+        'Avocados',
+        'Afternoon',
+        'Vegetable',
+        true,
+        '240'
+    ),
+    (
+        'Poptarts',
+        'Evening',
+        'Snack',
+        false,
+        '200'
+    ),
+    (
+        'Greek Yogurt',
+        'Morning',
+        'Dairy',
+        true,
+        '100'
+    );
