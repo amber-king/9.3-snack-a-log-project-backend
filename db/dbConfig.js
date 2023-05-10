0a7f4e209014081a13dfabb7cb3883c6662a8c0f
Ari-Jackson <112004676+Ari-Jackson@users.noreply.github.com>
2023-05-10 12:25:10 -0400

    Sets up connection for deployment

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -1,12 +1,19 @@
 const pgp = require("pg-promise")();
 require("dotenv").config();
 
-const cn = {
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER
-};
+const { DATABASE_URL, PG_HOST, PG_PORT, PG_DATABASE, PG_USER } = process.env;
+
+const cn = DATABASE_URL
+  ? {
+      connectionString: DATABASE_URL,
+      max: 30,
+    }
+  : {
+      host: PG_HOST,
+      port: PG_PORT,
+      database: PG_DATABASE,
+      user: PG_USER,
+    };
 
 const db = pgp(cn);
 
